90946955e0a8ad7a43aaa6a313762bde5d94832a
ramirocap2021 <88505122+ramirocap2021@users.noreply.github.com>
2021-08-12 15:24:08 -0500

    Update Database.java

--- src/main/java/com/pluralsight/order/util/Database.java
+++ src/main/java/com/pluralsight/order/util/Database.java
@@ -14,6 +14,8 @@ public class Database {
     private static Database instance = null;
     private static boolean isInitialized = false;
     private String url = "jdbc:h2:mem:orders;DB_CLOSE_DELAY=-1";
+    private String user = "sa";
+    private String password = "";
 
     /**
      * Private constructor
@@ -50,7 +52,7 @@ public class Database {
      * @throws SQLException In case of a database error
      */
     public Connection getConnection() throws SQLException {
-        Connection connection = null;
+        Connection connection = DriverManager(url + " " + user + " " + password);
 
         if(!isInitialized && connection != null) {
             initializeDatabase(connection);
